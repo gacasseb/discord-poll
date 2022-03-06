60940c595e476cd0c0f8cefb9ce8ca9411480989
Gabriel Casseb <gabriel.casseb@dealersites.com.br>
2022-03-06 19:23:16 -0300

    auth url removed

--- database/keyv.js
+++ database/keyv.js
@@ -1,7 +1,7 @@
 const Keyv = require('@keyv/mongo');
-const { mongo_password } = require('../config.json');
+const { database_url } = require('../config.json');
 
-const keyv = new Keyv(`mongodb+srv://sysadm:${mongo_password}@casseb.4tucv.gcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+const keyv = new Keyv(database_url);
 
 keyv.on('error', err => console.error('Keyv connection error:', err));
 
